05ff087cdbed89b35e999ac5683ff67a67701840
Ivan Kukic <i.kukic@levi9.com>
2024-09-12 12:41:26 +0200

    Fixed return string for game logic

--- RPSLS/RPSLS.Domain/Logic/GameLogic.cs
+++ RPSLS/RPSLS.Domain/Logic/GameLogic.cs
@@ -7,18 +7,18 @@ namespace RPSLS.Domain.Logic
 		public static string DetermineWinner(HandsignType player, HandsignType computer)
 		{
 			if (player == computer)
-				return "You tie";
+				return "tie";
 
 			return (player, computer) switch
 			{
-				(HandsignType.Rock, HandsignType.Scissors) or (HandsignType.Rock, HandsignType.Lizard) => "You win",
-				(HandsignType.Paper, HandsignType.Rock) or (HandsignType.Paper, HandsignType.Spock) => "You win",
-				(HandsignType.Scissors, HandsignType.Paper) or (HandsignType.Scissors, HandsignType.Lizard) => "You win",
-				(HandsignType.Lizard, HandsignType.Spock) or (HandsignType.Lizard, HandsignType.Paper) => "You win",
-				(HandsignType.Spock, HandsignType.Scissors) or (HandsignType.Spock, HandsignType.Rock) => "You win",
+				(HandsignType.Rock, HandsignType.Scissors) or (HandsignType.Rock, HandsignType.Lizard) => "win",
+				(HandsignType.Paper, HandsignType.Rock) or (HandsignType.Paper, HandsignType.Spock) => "win",
+				(HandsignType.Scissors, HandsignType.Paper) or (HandsignType.Scissors, HandsignType.Lizard) => "win",
+				(HandsignType.Lizard, HandsignType.Spock) or (HandsignType.Lizard, HandsignType.Paper) => "win",
+				(HandsignType.Spock, HandsignType.Scissors) or (HandsignType.Spock, HandsignType.Rock) => "win",
 
 				// All other cases are where computer wins
-				_ => "You lose"
+				_ => "lose"
 			};
 		}
 	}
